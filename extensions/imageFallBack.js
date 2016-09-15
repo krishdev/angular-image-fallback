ebe2bb67875cda6aef42885f0503795803a97d82
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2016-09-15 15:57:38 -0700

    code merge

---
+++ extensions/imageFallBack.js
@@ -0,0 +1,57 @@
+/**
+ * Angular img ngSrc Fallback
+ * (c) 2016 Krishnakumar Rajendran
+ * license: MIT 
+ * github: https://github.com/krishdev/imgFallBack
+ */
+
+(function () {
+    angular.module('imgFallback', [])
+        .factory('imgService', ['$http', '$location', function ($http, $location) {
+            return {
+                checkForPage: function (url) {
+                    return $http.head(url);
+                }
+            }
+    }])        
+        .directive('fallbackHide', ['imgService', function (imgService) {
+            return {
+                restrict: 'A',
+                link: function (scope, elem, attrs) {
+                    var imgSrc = attrs.ngSrc;
+                    var isPageExists = function () {
+                        imgService.checkForPage(imgSrc).then(function successCallback(response) {
+                            var responseTxt = response.status;
+                            if (responseTxt != 200) {
+                                elem.hide();
+                            }
+                        }, function errorCallback(response) {
+                            elem.hide();
+                        });
+                    }
+                    isPageExists();
+                }
+            };
+    }])
+    .directive('fallbackDefault', ['imgService', function (imgService) {
+            return {
+                restrict: 'A',
+                link: function (scope, elem, attrs) {
+                    var imgSrc = attrs.ngSrc;
+                    var isPageExists = function () {
+                        var defaultImg = "http://xpenology.org/wp-content/themes/qaengine/img/default-thumbnail.jpg";
+                        imgService.checkForPage(imgSrc).then(function successCallback(response) {
+                            var responseTxt = response.status;
+                            if (responseTxt != 200) {
+                                attrs.src = defaultImg;
+                            }
+                        }, function errorCallback(response) {
+                            elem.attr("src",defaultImg);
+                        });
+                    }
+                    isPageExists();
+                }
+            };
+    }]);
+
+})();
